adde31fa6211fdf59c1599b0cfaa45f6018e2109
Saad Malik <36926771+maliksh7@users.noreply.github.com>
2019-12-14 04:31:18 +0500

    Add files via upload
    This is a Case Study Of Object Oriented Programming via Python. In this case study we look upon concepts of inheritance,composition etc.

---
+++ Case Study of OOP via Python/oop.py
@@ -0,0 +1,80 @@
+class Address:
+	
+	def __init__(self, house_no = 0, street = 0, city = "", country = ""):
+		self.house_no = house_no
+		self.street = street
+		self.city = city
+		self.country = country
+
+	def get_full_address(self):
+		return "H. No. "+ str(self.house_no) + ", Street "+ str(self.street) + ", "+ self.city + " "+ self.country
+
+	def __str__(self):
+		return self.get_full_address()
+
+
+class Employee:
+	
+	def __init__(self, id = 1, name = None, current_address = None, permanent_address = None):
+		self.id = id
+		self.name = name
+		self.current_address = current_address
+		self.permanent_address = permanent_address
+
+	def set_current_address(self,house_no,street,city,country):
+		self.current_address = Address(house_no,street,city,country)
+
+	def set_permanent_address(self,house_no,street,city,country):
+		self.permanent_address = Address(house_no,street,city,country)
+
+	def get_current_address(self):
+		return str(self.current_address)
+
+	def get_permanent_address(self):
+		return str(self.permanent_address)
+	def __str__(self):
+		return str(self.id) + " " + str(self.name) + " " + self.get_permanent_address()
+                                         
+class Lecturer(Employee):
+	 
+	def __init__(self):
+		super().__init__(id = 1, name = "Mr. Bigshot")
+
+	def __str__(self):
+		return "Lecturer: "+ str(self.id)+ " "+ str(self.name)+ str(self.get_permanent_address())	
+
+
+
+
+
+
+
+if __name__ == "__main__": 
+    # Uncomment these to test out your code before run.py local 
+
+    
+   #  a = Address() 
+   #  a.house_no = 2 
+   #  a.street = 3 
+   #  a.city = "Peshawar"
+   #  a.country = "Pakistan"
+
+   #  print(a.get_full_address())
+   # # print(a)
+
+
+   #  e = Employee() 
+   #  e.set_current_address(1, 2, "Cape Town", "South Africa")
+   #  print(e.get_current_address()) 
+
+   #  e.set_permanent_address(4, 19, "Cape Town", "South Africa")
+   #  print(e.get_permanent_address())
+
+   #  print(e)
+
+    l = Lecturer() 
+    l.set_permanent_address(44, 24, "KL", "Malaysia")
+    print(l.get_permanent_address())
+    print(l)		
+
+
